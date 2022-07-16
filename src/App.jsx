019ed3b2327e5cc677eeb95b358db081ea1aa3a1
Mateus Marçal <moteus10@gmail.com>
2022-07-16 07:31:17 -0300

    Feat: Create Home Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
-
-  return (
-    <h1>oi</h1>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

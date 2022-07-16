fd872ee26f0801a5a3104bbc3550f8f09ba0eb3c
Mateus Marçal <moteus10@gmail.com>
2022-07-16 07:34:32 -0300

    Fix: Eslint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;

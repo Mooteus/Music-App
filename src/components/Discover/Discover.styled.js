816aacc41a0e12ebc83a7721402a360d54808e9c
Mateus Marçal <moteus10@gmail.com>
2022-07-19 13:45:24 -0300

    Feat: Add discover title

--- src/components/Discover/Discover.styled.js
+++ src/components/Discover/Discover.styled.js
@@ -1,9 +1,26 @@
 import styled from 'styled-components';
 
-const DiscoverContainer = styled.div`
+export const DiscoverContainer = styled.div`
   height: 100vh;
   width: 60%;
   padding-left: 50px;
+  padding-right: 50px;
 `;
 
-export default DiscoverContainer;
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h1`
+  font-size: 40px;
+  color: black;
+  font-weight: bold;
+`;
+
+export const MoreSettings = styled.span`
+  font-size: 45px;
+  color: gray;
+  cursor: pointer;
+`;
--- src/components/Discover/index.jsx
+++ src/components/Discover/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Search from '../Search';
-import DiscoverContainer from './Discover.styled';
+import
+{
+  DiscoverContainer,
+  TitleContainer,
+  Title,
+  MoreSettings,
+} from './Discover.styled';
 
 function Discover() {
   return (
     <DiscoverContainer>
       <Search />
+      <TitleContainer>
+        <Title>Discover</Title>
+        <MoreSettings>...</MoreSettings>
+      </TitleContainer>
     </DiscoverContainer>
   );
 }
